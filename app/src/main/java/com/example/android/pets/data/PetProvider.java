8b39e48eeb35a88aa6314ac588a1d144c9023d25
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-02-25 10:20:40 +0600

    query() method is defined

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -1,8 +1,11 @@
 package com.example.android.pets.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 /**
@@ -16,6 +19,14 @@ public class PetProvider extends ContentProvider {
     /**
      * Initialize the provider and the database helper object.
      */
+    private static final int PETS=100;
+    private static final int PET_ID=101;
+    private static final UriMatcher sUriMatcher=new UriMatcher(UriMatcher.NO_MATCH);
+    static
+    {
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY,PetContract.PATH_PETS,PETS);
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY,PetContract.PATH_PETS+"/#",PET_ID);
+    }
     private PetDbHelper mDbHelper;
     @Override
     public boolean onCreate() {
@@ -32,7 +43,28 @@ public class PetProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        return null;
+        SQLiteDatabase database=mDbHelper.getReadableDatabase();
+        Cursor cursor;
+        int match=sUriMatcher.match(uri);
+        switch (match)
+        {
+            case PETS:
+                cursor=database.query(PetContract.PetEntry.TABLE_NAME,projection,null,null,null,null,sortOrder);
+
+                break;
+
+            case PET_ID:
+                selection=PetContract.PetEntry._ID +"=?";
+                selectionArgs=new String[] { String.valueOf(ContentUris.parseId(uri))};
+                cursor=database.query(PetContract.PetEntry.TABLE_NAME,projection,selection,selectionArgs,null,null,sortOrder);
+
+                break;
+
+             default:
+                 throw new IllegalArgumentException("Cannot query unknown uri"+uri);
+
+        }
+        return cursor;
     }
 
     /**
